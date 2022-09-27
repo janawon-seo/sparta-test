834568fefe9d2c324080a5afb2c15e61b349b86c
jangwonseo <sjw887@naver.com>
2022-09-27 10:50:41 +0900

    Add introduce app

--- introduce/views.py
+++ introduce/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from introduce.models import AccessLog
 
+#case1
 def first_view(request):
     access_log = AccessLog()
     access_log.location = "introduce"
     access_log.save()
     return render(request, 'my_test.html')
 
+
+
+    #case2
+    #AccessLog.objects.create(
+    #    location="introduce"
+    #)
 # Create your views here.
